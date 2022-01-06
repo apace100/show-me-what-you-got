876bbfde220134e8d1f6322c893c20dcdb91b4de
Apace <apace100@web.de>
2022-01-06 19:20:55 +0100

    Prevented item sharing keybind when Calio is installed

--- src/main/java/io/github/apace100/smwyg/ShowMeWhatYouGotClient.java
+++ src/main/java/io/github/apace100/smwyg/ShowMeWhatYouGotClient.java
@@ -6,6 +6,11 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.fabricmc.loader.api.Version;
+import net.fabricmc.loader.api.VersionParsingException;
+import net.fabricmc.loader.impl.util.version.SemanticVersionImpl;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.util.InputUtil;
@@ -14,12 +19,26 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.slot.Slot;
 import org.lwjgl.glfw.GLFW;
 
+import java.util.Optional;
+
 public class ShowMeWhatYouGotClient implements ClientModInitializer {
 
     boolean sharedStack = false;
 
     @Override
     public void onInitializeClient() {
+        Optional<ModContainer> calioMod = FabricLoader.getInstance().getModContainer("calio");
+        if(calioMod.isPresent()) {
+            try {
+                Version highestCalioVersionWithItemSharing = Version.parse("1.4.2");
+                if(calioMod.get().getMetadata().getVersion().compareTo(highestCalioVersionWithItemSharing) <= 0) {
+                    ShowMeWhatYouGot.LOGGER.info("Calio <= 1.4.2 detected, turning off Show Me What You Got item sharing keybind.");
+                    return;
+                }
+            } catch (VersionParsingException e) {
+                ShowMeWhatYouGot.LOGGER.warn("Could not properly detect whether Calio was present. Possible side-effect: items are shared in chat twice.");
+            }
+        }
         ClientTickEvents.START_CLIENT_TICK.register(tick -> {
             MinecraftClient client = MinecraftClient.getInstance();
             if(client.player != null && client.currentScreen instanceof HandledScreen) {
