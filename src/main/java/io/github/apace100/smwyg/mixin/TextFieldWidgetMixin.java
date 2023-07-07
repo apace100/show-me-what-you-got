b9ac2446e8a7c7ee21b77a5812dceea010620a46
libjared <libjared@users.noreply.github.com>
2023-07-07 14:50:51 -0500

    Merge pull request #10
    * Prevent indexOutOfBounds, handle more chatbox edgecases
    
    * DRY mixin logic, adjust to cutting/pasting normal text
    
    * Remove some logging calls
    
    * Fix off-by-one in selection
    
    * Call it select instead of highlight
    
    * Remove TODOs

--- src/main/java/io/github/apace100/smwyg/mixin/TextFieldWidgetMixin.java
+++ src/main/java/io/github/apace100/smwyg/mixin/TextFieldWidgetMixin.java
@@ -1,6 +1,8 @@
 package io.github.apace100.smwyg.mixin;
 
 import io.github.apace100.smwyg.duck.ItemSharingTextFieldWidget;
+import net.fabricmc.loader.impl.util.log.Log;
+import net.fabricmc.loader.impl.util.log.LogCategory;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -15,7 +17,6 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(TextFieldWidget.class)
 public abstract class TextFieldWidgetMixin implements ItemSharingTextFieldWidget {
-
     @Shadow private String text;
 
     private ItemStack itemStack;
@@ -25,44 +26,64 @@ public abstract class TextFieldWidgetMixin implements ItemSharingTextFieldWidget
 
     @Override
     public void setStack(ItemStack stack) {
-        this.itemStack = stack;
+        itemStack = stack;
         Text text = stack.toHoverableText();
-        this.insertedString = text.getString();
-        this.insertedLength = insertedString.length();
+        insertedString = text.getString();
+        insertedLength = insertedString.length();
     }
 
     @Override
     public ItemStack getStack() {
-        return this.itemStack;
+        return itemStack;
     }
 
     @Override
     public String getTextBefore() {
-        return this.text.substring(0, insertedIndex);
+        if (insertedIndex < 0 || insertedIndex > text.length()) {
+            Log.warn(LogCategory.GENERAL,"ShowMeWhatYouGot: invalid tag positioning in getTextBefore");
+            Log.warn(LogCategory.GENERAL,
+                    "text=%s length=%d insertedIndex=%d insertedLength=%d",
+                    text, text.length(), insertedIndex, insertedLength);
+            return "";
+        }
+
+        return text.substring(0, insertedIndex);
     }
 
     @Override
     public String getTextAfter() {
-        return this.text.substring(insertedIndex + insertedLength);
+        if (insertedIndex + insertedLength < 0 || insertedIndex + insertedLength > text.length()) {
+            Log.warn(LogCategory.GENERAL,"ShowMeWhatYouGot: invalid tag positioning in getTextAfter");
+            Log.warn(LogCategory.GENERAL,
+                    "text=%s length=%d insertedIndex=%d insertedLength=%d",
+                    text, text.length(), insertedIndex, insertedLength);
+            return "";
+        }
+
+        return text.substring(insertedIndex + insertedLength);
     }
 
     @Override
     public boolean hasStack() {
-        return this.itemStack != null;
+        return itemStack != null;
     }
 
     @Override
     public void onSuggestionInserted(int start, int offset) {
-        if(this.itemStack != null && start <= insertedIndex) {
-            this.insertedIndex += offset;
+        if (this.hasStack() && start <= insertedIndex) {
+            insertedIndex += offset;
         }
     }
 
     @Inject(method = "getCursorPosWithOffset", at = @At("RETURN"), cancellable = true)
     private void modifyCursorOffset(int offset, CallbackInfoReturnable<Integer> cir) {
+        if (!this.hasStack()) {
+            return;
+        }
+
         int original = cir.getReturnValue();
-        if(original > insertedIndex && original < insertedIndex + insertedLength) {
-            if(offset < 0) {
+        if (original > insertedIndex && original < insertedIndex + insertedLength) {
+            if (offset < 0) {
                 cir.setReturnValue(insertedIndex);
             } else {
                 cir.setReturnValue(insertedIndex + insertedLength);
@@ -70,17 +91,45 @@ public abstract class TextFieldWidgetMixin implements ItemSharingTextFieldWidget
         }
     }
 
-    @Inject(method = "write", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setSelectionStart(I)V"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void moveInsertedText(String text, CallbackInfo ci, int i, int j, int k, String string, int l, String string2) {
-        if(i <= insertedIndex && j <= insertedIndex) {
-            this.insertedIndex += l;
+    @Inject(method = "getWordSkipPosition", at = @At("RETURN"), cancellable = true)
+    private void modifyWordSkipPosition(int wordOffset, CallbackInfoReturnable<Integer> cir) {
+        if (!this.hasStack()) {
+            return;
         }
+
+        int original = cir.getReturnValue();
+        if (original > insertedIndex && original < insertedIndex + insertedLength) {
+            if (wordOffset < 0) {
+                cir.setReturnValue(insertedIndex);
+            } else {
+                int skipPos = insertedIndex + insertedLength;
+                // when skipping forwards over a tag, keep going until we hit the beginning of the next word
+                // or the end of the text
+                while (skipPos < text.length() && text.charAt(skipPos) == ' ') {
+                    ++skipPos;
+                }
+                cir.setReturnValue(skipPos);
+            }
+        }
+    }
+
+    @Inject(method = "write", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setSelectionStart(I)V"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void moveInsertedText(String rawText, CallbackInfo ci, int start, int end, int spaceRemaining, String sanitizedText, int sanitizedTextLength, String newText) {
+        if (!this.hasStack()) {
+            return;
+        }
+
+        handleOverwrite(start, end, sanitizedTextLength);
     }
 
     @ModifyVariable(method = "setCursor", at = @At("HEAD"), argsOnly = true, ordinal = 0)
     private int modifyCursorSetting(int original) {
-        if(original > insertedIndex && original < insertedIndex + insertedLength) {
-            if(original <= insertedIndex + (insertedLength / 2)) {
+        if (!this.hasStack()) {
+            return original;
+        }
+
+        if (original > insertedIndex && original < insertedIndex + insertedLength) {
+            if (original <= insertedIndex + (insertedLength / 2)) {
                 return insertedIndex;
             } else {
                 return insertedIndex + insertedLength;
@@ -92,35 +141,61 @@ public abstract class TextFieldWidgetMixin implements ItemSharingTextFieldWidget
 
     @ModifyVariable(method = "setSelectionStart", at = @At("HEAD"), argsOnly = true, ordinal = 0)
     private int modifySelectionStart(int original) {
-        if(original > insertedIndex && original < insertedIndex + insertedLength) {
+        if (!this.hasStack()) {
+            return original;
+        }
+
+        if (original > insertedIndex && original < insertedIndex + insertedLength) {
             return insertedIndex;
         } else {
             return original;
-        }
-    }
-
-    @Inject(method = "eraseCharacters", at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;<init>(Ljava/lang/String;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void eraseInsertion(int characterOffset, CallbackInfo ci, int i, int j, int k) {
-        boolean isStartInside = j > insertedIndex && j < insertedIndex + insertedLength;
-        boolean isEndInside = k > insertedIndex && k < insertedIndex + insertedLength;
-        if(isStartInside || isEndInside || (j < insertedIndex && k >= insertedIndex + insertedLength)) {
-            this.itemStack = null;
-            this.insertedLength = 0;
-            this.insertedString = "";
-            this.insertedIndex = 0;
-        } else {
-            if(j <= insertedIndex || k <= insertedIndex) {
-                this.insertedIndex -= (k - j);
-            }
         }
     }
 
     @ModifyVariable(method = "setSelectionEnd", at = @At("HEAD"), argsOnly = true, ordinal = 0)
     private int modifySelectionEnd(int original) {
-        if(original > insertedIndex && original < insertedIndex + insertedLength) {
-            return insertedIndex;
+        if (!this.hasStack()) {
+            return original;
+        }
+
+        if (original > insertedIndex && original < insertedIndex + insertedLength) {
+            return insertedIndex + insertedLength;
         } else {
             return original;
         }
+    }
+
+    // all input actions are performed through calls to write(), except for backspace/delete when nothing is selected. handle that here.
+    @Inject(method = "eraseCharacters", at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;<init>(Ljava/lang/String;)V"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void eraseInsertion(int characterOffset, CallbackInfo ci, int index, int start, int end) {
+        if (!this.hasStack()) {
+            return;
+        }
+
+        handleOverwrite(start, end, 0);
+    }
+
+    private void handleOverwrite(int start, int end, int replacementLength) {
+        // when no text is selected, start == end
+
+        boolean isErasingEntireTag = start <= insertedIndex && end >= insertedIndex + insertedLength;
+        // selecting a range inside the tag is not typically possible, but handle it anyway
+        boolean isStartInside = start > insertedIndex && start < insertedIndex + insertedLength;
+        boolean isEndInside = end > insertedIndex && end < insertedIndex + insertedLength;
+        boolean isErasingBefore = start <= insertedIndex && end <= insertedIndex;
+
+        if (isErasingEntireTag || isStartInside || isEndInside) {
+            reset();
+        } else if (isErasingBefore) {
+            int offset = replacementLength - (end - start);
+            insertedIndex += offset;
+        }
+    }
+
+    private void reset() {
+        itemStack = null;
+        insertedLength = 0;
+        insertedString = null;
+        insertedIndex = 0;
     }
 }
